cde84ca9d5c586f012f3a56aab9d125802909930
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-09 13:58:25 -0500

    [FEAT]: Implementa endpoint de registro con AuthService y RegisterDTO

--- TruequeU/Controllers/AuthController.cs
+++ TruequeU/Controllers/AuthController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TruequeU.Interfaces;
+using TruequeU.Models.DTO;
 
 namespace TruequeU.Controllers
 {
@@ -11,6 +12,17 @@ namespace TruequeU.Controllers
         public AuthController(IAuthService authService)
         {
             _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO model)
+        {
+            var result = await _authService.Register(model.email, model.password, model.rol);
+
+            if (result.Succeeded)
+                return Ok(new { message = $"Usuario {model.email} creado correctamente" });
+
+            return BadRequest();
         }
 
         public IActionResult Index()
