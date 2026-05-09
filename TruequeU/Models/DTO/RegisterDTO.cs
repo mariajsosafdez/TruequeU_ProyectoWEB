98eeb1afcec1d64de85e23874519281dfa21261d
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-09 13:52:50 -0500

    [FEAT]: crea DTO para registro de usuarios

---
+++ TruequeU/Models/DTO/RegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeU.Models.DTO
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(6, ErrorMessage ="La contraseña debe tener al menos 6 caracteres")]
+        public string password { get; set; }
+        [Required(ErrorMessage = "El rol es obligatorio")]
+        public string rol { get; set; }
+    }
+}
