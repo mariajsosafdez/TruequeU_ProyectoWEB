e36593ba83889f9451907130afb5b790d57a16b9
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-09 13:37:29 -0500

    [FEAT]: implementa registro de usuarios usando Identity

--- TruequeU/Interfaces/IAuthService.cs
+++ TruequeU/Interfaces/IAuthService.cs
@@ -1,6 +1,9 @@
-﻿namespace TruequeU.Interfaces
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TruequeU.Interfaces
 {
     public interface IAuthService
     {
+        public Task<IdentityResult> Register(string email, string password, string rol);
     }
 }
--- TruequeU/Services/AuthService.cs
+++ TruequeU/Services/AuthService.cs
@@ -1,6 +1,39 @@
-﻿namespace TruequeU.Services
+﻿using Microsoft.AspNetCore.Identity;
+using TruequeU.Interfaces;
+
+namespace TruequeU.Services
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        public async Task<IdentityResult> Register(string email, string pw, string role)
+        {
+            var user = new IdentityUser
+            {
+                UserName = email,
+                Email = email
+            };
+            var result = await _userManager.CreateAsync(user, pw);//por defecto ya cifra la contraseña
+            if (result.Succeeded)
+            {
+                if (!await _roleManager.RoleExistsAsync(role))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(role));//si no está el rol lo crea .-.
+                    //la buena práctica sería tener eso separado en endpoints
+                }
+                await _userManager.AddToRoleAsync(user, role); //al usuario agregale el rol :)
+            }
+            return result;
+        }
     }
 }
