1d0cb321617c2fba3d9fec5c19850502ab7beae0
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-09 14:09:07 -0500

    [FEAT]: crea DTO para inicio de sesión

---
+++ TruequeU/Models/DTO/LoginDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeU.Models.DTO
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "El email está vacío")]
+        [EmailAddress(ErrorMessage = "Formato inválido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "La contraseña está vacía")]
+        public string Password { get; set; }
+    }
+}
