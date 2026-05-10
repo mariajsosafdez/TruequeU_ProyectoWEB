069ae3d189b828f8f8cb88076e13df905a63863b
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-10 11:59:31 -0500

    [FEAT]: Añade tabla Clients al DbContext

--- TruequeU/Persistence/ApplicationDbContext.cs
+++ TruequeU/Persistence/ApplicationDbContext.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TruequeU.Models;
 
 namespace TruequeU.Persistence
 {
     public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
 
-        }
+        public DbSet<Clients> Clients { get; set; }
     }
 }
