b7af09f27ba199d6410cfae1aa5ad80935b42162
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-10 11:56:59 -0500

    [FEAT]: Agrega modelo de Cliente y DTO para pedir solo lo necesario al crearlo

---
+++ TruequeU/Models/Clients.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TruequeU.Models
+{
+    public class Clients
+    {
+        [Key]
+        public Guid IdCliente { get; set; } = Guid.NewGuid();
+        [Required]
+        public string NombreCliente { get; set; }
+        public string Carrera { get; set; }
+        public float Puntuacion { get; set; } = 0;
+        public bool IsActive { get; set; } = true;
+        [Required]
+        public string IdentityUserId { get; set; } = string.Empty;
+        [ForeignKey("IdentityUserId")]
+        public IdentityUser? IdentityUser { get; set; }
+    }
+}
---
+++ TruequeU/Models/DTO/ClientCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TruequeU.Models.DTO
+{
+    public class ClientCreateDTO
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres")]
+        public string NombreCliente { get; set; }
+        public string CarreraCliente { get; set; }
+        [Required(ErrorMessage ="El IdentityUserId es obligatorio")]
+        public string IdentityUserId { get; set; } = string.Empty;
+    }
+}
