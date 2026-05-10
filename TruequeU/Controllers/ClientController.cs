3c7754015ce3862a78b6580d3bde47051e3dea3c
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-10 12:24:41 -0500

    [CHORE]: crea la estructura inicial para ClientController

---
+++ TruequeU/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TruequeU.Interfaces;
+
+namespace TruequeU.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ClientController : Controller
+    {
+        private readonly IClientService _clientService;
+
+        public ClientController(IClientService clientService)
+        {
+            _clientService = clientService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
