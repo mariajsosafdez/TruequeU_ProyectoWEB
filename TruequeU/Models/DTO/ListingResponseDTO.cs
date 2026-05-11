b0c0890945ef74934d36dd5d83e73f139e9e4601
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-11 13:47:56 -0500

    [FIX]: llena el ListingResponseDTO que se había vaciado accidentalmente, corrige nombre de atributo en el Service

--- TruequeU/Models/DTO/ListingResponseDTO.cs
+++ TruequeU/Models/DTO/ListingResponseDTO.cs
@@ -1,6 +1,15 @@
-﻿namespace TruequeU.Models.DTO
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeU.Models.DTO
 {
-    public class ListingSummaryDTO
+    public class ListingResponseDTO
     {
+        public Guid IdListing { get; set; }
+        public string Titulo { get; set; }
+        public Condition Condicion { get; set; }
+        public Status Estado { get; set; }
+        public Category Categoria { get; set; }
+        public float Precio { get; set; }
+        public string OwnerName { get; set; }
     }
 }
