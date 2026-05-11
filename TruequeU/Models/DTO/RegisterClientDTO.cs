9821dcc58795fbe080f9a569da7c55d8455ba2ed
juliMR-23 <julian.marin42@eia.edu.co>
2026-05-11 12:03:04 -0500

    [FEAT]: crea función para registrar User/Client a la par (+DTO para usar los campos necesarios)

---
+++ TruequeU/Models/DTO/RegisterClientDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TruequeU.Models.DTO
+{
+    public class RegisterClientDTO
+    {
+        [Required(ErrorMessage = "El email es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de email inválido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "La contraseña es obligatoria")]
+        [MinLength(6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres")]
+        public string NombreCliente { get; set; }
+        public string CarreraCliente { get; set; } = string.Empty;
+    }
+}
