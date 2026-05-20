f43fa7f5d98461b0ee0a4ac8ccf036973f37d6c8
Maria José Sosa <maria.sosa15@eia.edu.co>
2026-05-20 07:50:33 -0500

    [FEAR]: Ultimas migraciones

--- TruequeU/Migrations/20260520044007_AddListingImageModel.Designer.cs
+++ TruequeU/Migrations/20260520044007_AddListingImageModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TruequeU.Persistence;
 
@@ -11,9 +12,11 @@ using TruequeU.Persistence;
 namespace TruequeU.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260520044007_AddListingImageModel")]
+    partial class AddListingImageModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TruequeU/Migrations/20260520044007_AddListingImageModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TruequeU.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddListingImageModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListingImages",
+                columns: table => new
+                {
+                    ImageID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Url = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ListingID = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListingImages", x => x.ImageID);
+                    table.ForeignKey(
+                        name: "FK_ListingImages_Listings_ListingID",
+                        column: x => x.ListingID,
+                        principalTable: "Listings",
+                        principalColumn: "IdListing");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ListingImages_ListingID",
+                table: "ListingImages",
+                column: "ListingID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListingImages");
+        }
+    }
+}
